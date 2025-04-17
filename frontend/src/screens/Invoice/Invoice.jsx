68f692cfdcf792c63fe6e5bbe908595c0e04c8d3
Thassaporn <PorHunKook@gmail.com>
2025-04-18 01:27:40 +0700

    png log

--- frontend/src/screens/Invoice/Invoice.jsx
+++ frontend/src/screens/Invoice/Invoice.jsx
@@ -74,6 +74,11 @@ export const Invoice = () => {
 
 
       const link = document.createElement("a");
+      console.log("Preparing download…", {
+        id,                                         
+        fileName: `IV${id}.png`,                       
+        imgSizeKB: (imgData.length * 3) / 4 / 1024  
+      });
       link.href = imgData;
       link.download = `IV${id}.png`;  
       link.click();
